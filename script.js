514fbcecff28b8ee5d1df38857e6208f5417dc6b
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-06-28 15:38:07 +0530

    ~Barsha

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ window.onresize = function(event) {
 var href = window.location.href.split("/")
 var html_location = href[href.length-1]
 
-if (window.innerWidth >= 760 && html_location !== "index.html") {
+if (window.innerWidth >= 440 && html_location !== "index.html") {
     window.location - "index.html"
 }
-if (window.innerWidth < 760 && html_location !== "index2.html") {
+if (window.innerWidth < 460 && html_location !== "index2.html") {
     window.location - "index2.html"
 }
